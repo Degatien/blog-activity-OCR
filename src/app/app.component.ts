547d8c39c9f0b48f3882088c5aad3dc8628c0da7
Degatien <degatien@gmail.Com>
2019-08-23 17:53:51 +0200

    [ADD] PostList in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import {Post} from './helper/Post'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog';
+  post = [
+    new Post('Premier post', 'Ceci est le premier post, neutre', 0, new Date()),
+    new Post('Second post', 'Ceci est le second post, très apprécié', 16, new Date()),
+    new Post('Troisième post', 'Ceci est le troisième post, très controversé', -9, new Date())
+  ];
+
 }
