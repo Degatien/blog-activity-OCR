7195616551abead00740065434e9259083323913
Degatien <degatien@gmail.Com>
2019-08-23 17:42:39 +0200

    [ADD] Post type

---
+++ src/app/helper/Post.ts
@@ -0,0 +1,12 @@
+class Post {
+  title: string;
+  content: string;
+  loveIts: number;
+  createdAt: Date;
+  constructor(title= 'Post', content= 'Contenu du post', loveIts= 0, createdAt= new Date()) {
+    this.title = title;
+    this.content = content;
+    this.loveIts = loveIts;
+    this.createdAt = createdAt;
+  }
+}
